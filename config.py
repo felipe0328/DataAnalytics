edd0fd9f259e245a8ca13eb90457091549bdd200
Felipe Puentes <feliipepuentes@gmail.com>
2023-07-15 12:26:53 -0500

    creating config file, updating requirements

---
+++ config.py
@@ -0,0 +1,2 @@
+DATABASE_URL="data-managment-db.cdsoq1tqo8hf.us-east-1.rds.amazonaws.com"
+LAMBDA_ENDPOINT="https://bxfmcf6d7rxlea64sn7wcvx5my0vjpjj.lambda-url.us-east-1.on.aws/"
---
+++ services/ExternalData.py
@@ -0,0 +1,7 @@
+import config
+import requests
+
+def obtain_dataset():
+    dataset_request = requests.get(config.LAMBDA_ENDPOINT)
+    if dataset_request.status_code == 200:
+        return dataset_request.content.decode()
